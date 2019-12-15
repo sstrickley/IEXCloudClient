cf7d4907f9b157cbd2f8a6866e590aae36fe4557
sstrickley <sstrickley3@gmail.com>
2019-12-15 06:49:38 -0500

    Initial Commit

---
+++ IEXCloudClient/Common/RequestTypes.cs
@@ -0,0 +1,16 @@
+﻿using IEXClient.Chart;
+using IEXClient.Dividends;
+using IEXClient.KeyStats;
+using IEXClient.Quote;
+using System.Collections.Generic;
+
+namespace IEXCloudClient.Common
+{
+    public class RequestTypes
+    {
+        public List<ChartResponse> chart { get; set; }
+        public KeyStatsResponse stats { get; set; }
+        public List<DividendResponse> dividends { get; set; }
+        public QuoteResponse quote { get; set; }
+    }
+}
